e78b92fe7fbdef7d9f3f06d8038fe2cf48ffb324
Aiden Lee <hoo8799@gmail.com>
2023-03-07 00:55:26 +0900

    test: pr test (#1)
    * fix: User Email uuid 로 변경
    
    * feat: prisma middleware 추가
    
    * chore: @faker-js/faker 패키지 설치
    
    * feat: createUser API 추가
    
    * feat: createUser API Prisma 타입 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('user')
+  createUser(
+    @Body() body?: Prisma.UserCreateInput,
+  ): Promise<User | { count: number }> {
+    return this.appService.createUser(body);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,55 @@
+import { faker } from '@faker-js/faker';
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createUser(
+    body?: Prisma.UserCreateInput,
+  ): Promise<User | { count: number }> {
+    // const newUser = await this.prisma.user.create({
+    //   data: {
+    //     name: faker.name.firstName(),
+    //     email: faker.datatype.uuid(),
+    //     profile: faker.lorem.sentences(),
+    //     userInfo: {
+    //       create: {
+    //         height: '174',
+    //         weight: 74,
+    //         address: faker.address.city(),
+    //       },
+    //     },
+    //   },
+    //   include: {
+    //     userInfo: true,
+    //   },
+    // });
+
+    // const data = new Array(100).fill({}).map((_) => ({
+    //   name: faker.name.firstName().slice(0, 9),
+    //   email: faker.datatype.uuid(),
+    //   profile: faker.lorem.sentences(),
+    // }));
+
+    // const newUser: { count: number } = await this.prisma.user.createMany({
+    //   data,
+    // });
+
+    const newUser: User = await this.prisma.user.create({
+      data: {
+        name: body.name,
+        email: faker.datatype.uuid(),
+        profile: body.profile,
+      },
+    });
+
+    return newUser;
   }
 }
